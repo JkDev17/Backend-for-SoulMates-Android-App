48c650753e46ca3077cfe42a45be1a4e480e5812
USER <jimkapadoukas@gmail.com>
2023-12-18 13:22:48 +0200

    Initial commit.

---
+++ src/main/java/com/jk/soulmatesbackendandroidapp/SoulmatesBackendAndroidAppApplication.java
@@ -0,0 +1,13 @@
+package com.jk.soulmatesbackendandroidapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulmatesBackendAndroidAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulmatesBackendAndroidAppApplication.class, args);
+	}
+
+}

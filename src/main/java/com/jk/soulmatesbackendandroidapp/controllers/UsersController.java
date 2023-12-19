c22a0e22feab1f7205da611b20b6a2df71a78fc8
JkDev17 <jimkapadoukas@gmail.com>
2023-12-19 17:22:40 +0200

    First users endpoint created: createdAll
    This createAll endpoint creates massively users. It gets the images for the users from the url "https://picsum.photos/300/300?random". The 300/300 are the images size. What must be explained is the httpclient. It uses the method setRedirectStrategy(new LaxRedirectStrategy()).build() since we need to redirect to get the actual image(The url "https://picsum.photos/300/300?random" redirects us to a new url which has the image). Once that is completed we save the image.
    
    Next steps: With the library Faker get data for the firstname, lastname, location & along with the images save the user to our db.

--- src/main/java/com/jk/soulmatesbackendandroidapp/controllers/UsersController.java
+++ src/main/java/com/jk/soulmatesbackendandroidapp/controllers/UsersController.java
@@ -1,26 +1,54 @@
 package com.jk.soulmatesbackendandroidapp.controllers;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.util.EntityUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+
+/**
+ * Api for user creation.
+ * This api will not be protected since it's cause is only the user creation.
+ *
+ * @author Jk
+ * @since 19-12-2023
+ */
 @RestController
 @RequestMapping("/api/users")
 public class UsersController {
 
+    /**
+     * This endpoint creates users.
+     * We call the uri "<a href="https://picsum.photos/300/300?random">...</a>" to get the images for the users.
+     * It returns random images with the sizes of 300/300 as we declared.
+     * Notice the initialization of the httpClient with the method setRedirectStrategy(new LaxRedirectStrategy()).build().
+     * This happens since we need to redirect to get the actual image. --> try the url in a browser and you will understand.
+     *
+     * @author Jk
+     * @since 19-12-2023
+     */
     @PostMapping("/createAll")
-    private Boolean InsetUsers(){
+    private void insertUsers() throws IOException {
 
-
-        return true;
+        for (int i = 0; i < 30; i++) {
+            HttpClient httpClient = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build();
+            HttpGet request = new HttpGet("https://picsum.photos/300/300?random");
+            HttpResponse response = httpClient.execute(request);
+            System.out.println(response.getEntity());
+            byte[] imageBytes = EntityUtils.toByteArray(response.getEntity());
+            System.out.println(Arrays.toString(imageBytes));
+            Path imagePath = Path.of("C:\\Users\\USER\\Desktop\\" + i + ".jpg");
+            Files.write(imagePath, imageBytes, StandardOpenOption.CREATE);
+        }
     }
-
-    @PostMapping("/create")
-    private Boolean insertUser(){
-        //Somehow get the image and save the user
-        return true;
-    }
-
-
-
 }
--- src/main/java/com/jk/soulmatesbackendandroidapp/models/Users.java
+++ src/main/java/com/jk/soulmatesbackendandroidapp/models/Users.java
@@ -14,8 +14,40 @@ public class Users {
             generator = "id"
     )
     private long id;
-    private String firstName;
-    private String lastName;
-    private String location;
-    private byte[] image;
+    private final String firstName;
+    private final String lastName;
+    private final String location;
+    private final byte[] image;
+
+    public Users(long id, String firstName, String lastName, String location, byte[] image) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.location = location;
+        this.image = image;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
 }
